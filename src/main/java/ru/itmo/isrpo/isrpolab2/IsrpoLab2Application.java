ce6b67fcb7b21d062c090185f1ce56a1da191578
a.p.goryunov <a.p.goryunov@tinkoff.ru>
2026-03-09 00:07:21 +0300

    init

---
+++ src/main/java/ru/itmo/isrpo/isrpolab2/IsrpoLab2Application.java
@@ -0,0 +1,13 @@
+package ru.itmo.isrpo.isrpolab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsrpoLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsrpoLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/isrpo/isrpolab2/IsrpoLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.isrpo.isrpolab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsrpoLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
